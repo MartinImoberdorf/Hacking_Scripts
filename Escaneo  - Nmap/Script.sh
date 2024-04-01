7f34916d0310a3d9c3fe05f9bd24a6b3407c9464
Martín José Imoberdorf <93394695+MartinImoberdorf@users.noreply.github.com>
2024-04-01 00:21:44 -0300

    Add files via upload

---
+++ Escaneo  - Nmap/Script.sh
@@ -0,0 +1,73 @@
+#!/bin/bash
+
+# Escaneo silenicioso - lento
+function escaneoSilencioso(){
+nmap -sS -T0 -p- -Pn -n "$1" > escaneo_silencioso 2>/dev/null
+echo
+echo -e "\e[1;32mEscaneo completado y genereado el archivo 'escaneo_silencioso' \e[0m"
+echo
+}
+
+# Escaneo medio
+function escaneoMedio(){
+nmap -sS -sC -sV -p- -n -Pn -open -vvv --min-rate 5000 "$1" > escaneo_medio 2>/dev/null
+echo
+echo -e "\e[1;32mEscaneo completado y genereado el archivo 'escaneo_medio' \e[0m"
+echo
+}
+
+# Escaneo ruidoso - rapido
+function escaneoRuidoso(){
+nmap -sS -sV -sC -p- -T5 -v -A -PN -d --script-args=unsafe=1 "$1" > escaneo_ruidoso 2>/dev/null
+echo
+echo -e "\e[1;32mEscaneo completado y genereado el archivo 'escaneo_ruidoso' \e[0m"
+echo
+}
+
+# Ayuda
+function ayuda(){
+echo
+echo -e "\e[1;37mEl primer parametro debe contener la IP a escanear.\e[0m"
+echo
+}
+
+# Error
+function error(){
+echo
+echo -e "\e[1;31mOpción incorrecta. Por favor, seleccione una opción válida.\e[0m"
+echo
+}
+
+echo -e "\e[1;34m
+███████╗ ██████╗ █████╗ ███╗   ██╗   ████████╗██╗   ██╗██████╗ ███████╗
+██╔════╝██╔════╝██╔══██╗████╗  ██║   ╚══██╔══╝╚██╗ ██╔╝██╔══██╗██╔════╝
+███████╗██║     ███████║██╔██╗ ██║█████╗██║    ╚████╔╝ ██████╔╝█████╗  
+╚════██║██║     ██╔══██║██║╚██╗██║╚════╝██║     ╚██╔╝  ██╔═══╝ ██╔══╝  
+███████║╚██████╗██║  ██║██║ ╚████║      ██║      ██║   ██║     ███████╗
+╚══════╝ ╚═════╝╚═╝  ╚═╝╚═╝  ╚═══╝      ╚═╝      ╚═╝   ╚═╝     ╚══════╝
+By Imoberdorf Martin
+\e[0m"
+echo
+                                                                 
+if [ $# -eq 1 ]
+then
+while true
+do
+echo "1) Escaneo silenicioso - lento"
+echo "2) Escaneo medio"
+echo "3) Escaneo ruidoso - rapido"
+echo "4) Salir. "
+read -p "Seleccione el tipo de escaneo a realizar: " tipo
+echo
+case $tipo in
+1) escaneoSilencioso;;
+2) escaneoMedio;;
+3) escaneoRuidoso;;
+4) break;;
+*) error;;
+esac
+done
+else
+echo -e "\e[1;31mNo se dieron los parametros correctos.\e[0m"
+ayuda
+fi
